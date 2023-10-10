ce139000e3571d6b2836ac16526bc4a75afec399
Ciallo114514 <2798416994@qq.com>
2023-10-10 20:10:33 +0800

    second lesson

--- game/views.py
+++ game/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    line1 = '<h1 style="text-align:center">术士之战</h1>'
+    line2 = '<img src = "https://img0.baidu.com/it/u=2857096089,2095994689&fm=253&fmt=auto&app=138&f=JPEG?w=1070&h=500">'
+
+    return HttpResponse(line1 + line2)
+
+
+
