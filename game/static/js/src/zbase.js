6a81110dcaeab3a643153939c922cb8d5e24af32
Ciallo114514 <2798416994@qq.com>
2023-11-01 22:42:20 +0800

    third lesson

--- game/static/js/src/zbase.js
+++ game/static/js/src/zbase.js
@@ -5,5 +5,10 @@ class AcGame
             this.id = id;
             this.$ac_game = $('#' + id);
             this.menu = new AcGameMenu(this);
+            this.playground = new AcGamePlayground(this);
+            this.start();
+    }
+
+    start(){
     }
 }
